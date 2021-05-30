6a55174c36076dfeff5ae5f279e6656bb38241c2
sean <sean.snow@live.com>
2021-05-30 13:54:55 +0800

    feat: 1.0.4 获取request ip

---
+++ src/main/java/tech/aomi/common/web/util/RequestUtil.java
@@ -0,0 +1,36 @@
+package tech.aomi.common.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * HttpServletRequest 工具包
+ *
+ * @author Sean createAt 2021/5/30
+ */
+public class RequestUtil {
+
+    /**
+     * 获取真实IP
+     *
+     * @param request 请求信息
+     * @return ip地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-real-ip");//先从nginx自定义配置获取
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("x-forwarded-for");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+
+}
